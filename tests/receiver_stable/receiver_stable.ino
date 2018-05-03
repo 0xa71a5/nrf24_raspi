6b0481d6d2f8e44997fb5449f25f3ed38586a1f0
at1a5-lxc <anrist@sohu.com>
2018-05-03 17:49:01 +0800

    ENH: More feature for reliable comm

--- tests/receiver_stable/receiver_stable.ino
+++ tests/receiver_stable/receiver_stable.ino
@@ -10,21 +10,6 @@ void printf_begin(void)
   fdevopen( &serial_putc, 0 );
 }
 
-/* If reliable send success then return true ,otherwise return false */
-bool nrf_reliable_send(uint8_t *data, uint32_t length = 32, uint32_t max_fail_time = 100)
-{
-  uint8_t tx_status = 0x00;
-  uint8_t failed_times = 0;
-
-  while( nrf_carrier_detect() );
-  do {
-        tx_status = nrf_send(data);
-        failed_times ++;
-  }
-  while (tx_status != TX_REACH_DST && failed_times < max_fail_time);
-  return tx_status == TX_REACH_DST;
-}
-
 void setup()
 {
   Serial.begin(115200);
@@ -46,7 +31,7 @@ uint32_t comm_sum = 0;
 uint32_t last_check_sum = 0;
 uint32_t last_check_time = 0;
 
-uint8_t last_seq_num = 0;
+uint8_t last_seq_num[10] = {0}; /* Care that only 10 node addr allowed! */
 uint8_t data[32];
 uint32_t print_count = 0;
 
@@ -55,7 +40,10 @@ uint32_t print_count = 0;
 
 #define MAX_RETRY_TIME 10
 
-
+uint8_t seq_num = 0;
+uint8_t send_id = 0;
+uint8_t crc = 0;
+uint8_t crc_check = 0;
 void loop()
 {
   if (nrf_data_ready()) {
@@ -65,16 +53,27 @@ void loop()
       last_check_sum = comm_sum;
       last_check_time = millis();
     }
-    //if(print_count ++ % 50 == 0)
-      printf("Recv[%3u]->%s [%u B/s]\n", (uint8_t)data[31], (char *)data, comm_rate);
-    if ((uint8_t)data[31] == last_seq_num)
-      printf("Repeat seq num!\n");
-    else if ((uint8_t)data[31] != ((last_seq_num + 1) % 256))
-      printf("##Lost frame! cur=%u last=%u\n", (uint8_t)data[31], last_seq_num);
-    last_seq_num = data[31];
-    
-    comm_sum += 32;
 
+    send_id = data[29] < 10 ? data[29] : 0x00;
+    seq_num = data[30];
+    crc = data[31];
+    crc_check = crc_calculate(data, 31);
+
+    if (crc == crc_check) {
+      //if(print_count ++ % 50 == 0)
+        printf("Recv[%u] [%3u] [%4u B/s]\n", send_id, (uint8_t)seq_num, comm_rate);
+      if ((uint8_t)seq_num == last_seq_num[send_id])
+        printf("[%u]Repeat seq num %u!\n", send_id, seq_num);
+      else if ((uint8_t)seq_num != ((last_seq_num[send_id] + 1) % 256))
+        printf("[%u]Lost frame! cur=%u last=%u############\n", send_id, (uint8_t)seq_num, last_seq_num[send_id]);
+      last_seq_num[send_id] = seq_num;
+      
+      comm_sum += 32;
+    }
+    else {
+      printf("[%u] [%3u]Wrong crc 0x%02X != 0x%02X\n", send_id, seq_num, data, crc, crc_check);
+    }
+  
   }
   
 }
--- tests/sender2_stable/sender2_stable.ino
+++ tests/sender2_stable/sender2_stable.ino
@@ -19,8 +19,8 @@ void setup()
   Serial.println("Begin config!");
   nrf_gpio_init(8, 9); //Set ce pin and csn pin
   nrf_set_tx_addr((uint8_t *)"mac01");
-  nrf_set_rx_addr((uint8_t *)"mac00");
-  send_id = 0x00;
+  nrf_set_rx_addr((uint8_t *)"mac02");
+  send_id = 0x02;
   nrf_chip_config(12, 32); // Set channel and payload
   nrf_set_retry_times(5);
   nrf_set_retry_durtion(1250);
@@ -33,7 +33,6 @@ int start = 0;
 uint8_t seq_num = 0;
 uint8_t failed_times = 0;
 uint32_t loop_time = 0;
-
 #define MAX_RETRY_TIME 10
 void loop()
 {
@@ -45,20 +44,18 @@ void loop()
    data[31] = crc_calculate(data, 31);
    
    printf("[%u] [%3u] Send\n", send_id, seq_num);
-   
    if (!nrf_reliable_send(data))
       printf("Send failed!\n");
-   delay(20);
+   delay(10);
    if (tx_status != TX_REACH_DST)
       printf("Send failed with too many retries\n");
 
-   if(loop_time ++ % 10 == 0)
-      delay(random(1000));
-
    if (nrf_data_ready()) {
       nrf_get_data(data);
-   //   printf("Recv->%s\n\n", data);
+      //printf("Recv->%s\n\n", data);
   }
-  
+
+  if(loop_time ++ % 10 == 0)
+      delay(random(1000));
 }
 
